a6041e818f446e312cca1eb1c97bb7c576c00038
thiagoluppi <thiago.luppi@icloud.com>
2021-10-12 16:55:17 -0300

    commit aula_formulario_cadastro_3

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -27,3 +27,33 @@ end
 Entao("vejo a mensagem de alerta: Oops. Informe seu nome completo") do
   expect(find(".alert-dark").text).to eql "Oops. Informe seu nome completo!"
 end
+
+Quando('submeto meu cadastro sem o email') do
+  find("#fullName").set "Thiago Messias Luppi"
+  find("#password").set "pwd123"
+
+  click_button "Cadastrar"
+end
+
+Entao('vejo a mensagem de alerta: Oops. Informe um email válido!') do
+  expect(find(".alert-dark").text).to eql "Oops. Informe um email válido!"
+end
+
+Quando('submeto meu cadastro com email incorreto') do
+  find("#fullName").set "Thiago Messias Luppi"
+  find("#email").set "123"
+  find("#password").set "pwd123"
+
+  click_button "Cadastrar"
+end
+
+Quando('submeto meu cadastro sem a senha') do
+  find("#fullName").set "Thiago Messias Luppi"
+  find("#email").set Faker::Internet.free_email
+
+  click_button "Cadastrar"
+end
+
+Entao('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do
+  expect(find(".alert-dark").text).to eql "Oops. Informe sua senha secreta!"
+end
